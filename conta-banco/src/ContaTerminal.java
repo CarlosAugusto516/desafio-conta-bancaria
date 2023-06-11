fdedf0f7503f806e91703cf40e8028a830fa192b
Carlos Augusto <c.augusto516@gmail.com>
2023-06-11 12:54:39 -0300

    projeto concluído.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner (System.in);
+
+        String agencia;
+        int numero;
+        String nomeCliente = "Carlos Augusto";
+        double saldo = 237.48;
+
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = sc.next();
+        System.out.println("Por favor, digite o número da Conta!");
+        numero = sc.nextInt();
+
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+
     }
 }
